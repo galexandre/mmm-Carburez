05b4e8b4b939727bb6700aab973ad4b67892b823
Corentin <corentin.clement.pro@gmail.com>
2014-12-01 15:58:05 +0100

    Interface localization

--- src/main/java/service/LocalizationInterface.java
+++ src/main/java/service/LocalizationInterface.java
@@ -1,10 +1,18 @@
 package service;
 
+import dto.Position;
+
 /**
  * Interface gérant la localisation de l'utilisateur
  * @author corentin
  *
  */
 public interface LocalizationInterface {
+	/**
+	 * Retourne la position actuelle
+	 * @return La position sous forme d'objet Position
+	 */
+	Position getCurrentPosition();
+	
 	
 }
