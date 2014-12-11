f058ce73fe50850ce5469ef3be3c881e6dbcaeca
Corentin <corentin.clement.pro@gmail.com>
2014-12-11 20:11:57 +0100

    Affichage de l'utilisateur sur la carte

--- src/main/java/fr/istic/project/gasLocation/activities/MapSectionFragment.java
+++ src/main/java/fr/istic/project/gasLocation/activities/MapSectionFragment.java
@@ -5,20 +5,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import fr.istic.project.gasLocation.R;
 
 public class MapSectionFragment extends Fragment{
 
     public static final String ARG_SECTION_NUMBER = "section_number";
     
-//    private MapView mapView;
+    private GoogleMap map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_section_map, container, false);
         
-//        mapView = (MapView)rootView.findViewById(R.id.map);
+        map = ((SupportMapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+        // affichage de l'utilisateur sur la map
+        map.setMyLocationEnabled(true);
         
         return rootView;
     }
