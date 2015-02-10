fbebc5ee824187670a33ba9b8683f6ed1c8e62c0
Mathieu <mathieu.sachet@gmail.com>
2015-02-10 21:54:17 +0100

    Modification de la liste
    Ajout d’un adapter pour la liste des stations. Remplissage de la liste
    par des données en dur.

--- src/main/java/fr/istic/project/gasLocation/adapter/ListSectionFragment.java
+++ src/main/java/fr/istic/project/gasLocation/adapter/ListSectionFragment.java
@@ -1,57 +1,93 @@
 package fr.istic.project.gasLocation.adapter;
 
-import fr.istic.project.gasLocation.R;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
+import fr.istic.project.gasLocation.R;
 
 public class ListSectionFragment extends ListFragment {
 
+	public ListView list_stations;
+	private TextView stationName;
+	private TextView stationAdress;
+	private TextView gazolePrice;
+	private TextView essencePrice;
+	final String TAG_STATIONNAME = "stationName";
+	final String TAG_STATIONADRESS = "stationAdress";
+	final String TAG_GAZOLEPRICE = "gazolePrice";
+	final String TAG_ESSENCEPRICE = "essencePrice";
+	HashMap<String, Object> map = new HashMap<String, Object>();
+	
+	ArrayList<HashMap<String, Object>> stationsList = new ArrayList<HashMap<String, Object>>();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_section_list, container, false);
-
-		// Get ListView object from xml
-		final ListView list_stations; 
-		//list_stations = (ListView) findViewById(R.id.list_stations);
-
-		// Defined Array values to show in ListView
-		String[] values = new String[] { "Android List View", 
-				"Adapter implementation",
-				"Simple List View In Android",
-				"Create List View Android", 
-				"Android Example", 
-				"List View Source Code", 
-				"List View Array Adapter", 
-				"Android Example List View" 
-		};
-
-		// Define a new Adapter
-		// First parameter - Context
-		// Second parameter - Layout for the row
-		// Third parameter - ID of the TextView to which the data is written
-		// Forth - the Array of data
-
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, values);
+		
+		stationName = (TextView) getActivity().findViewById(R.id.stationName);
+		stationAdress = (TextView) getActivity().findViewById(R.id.stationAdress);
+		gazolePrice = (TextView) getActivity().findViewById(R.id.gazolePrice);
+		essencePrice = (TextView) getActivity().findViewById(R.id.essencePrice);
+		
 
 
-		// Assign adapter to ListView
-		setListAdapter(adapter); 
-
-		// ListView Item Click Listener
 		return rootView;
 	}
 
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+
+		String stationNames[] = {"Total Access", "Super U", "Carrefour" , "E.Leclerc", "Total Access", "ELF", "Avia", "Total"};
+		String stationAdresss[] = {"11 rue du Général Leclerc", "6 rue de la gare", "11 rue de l'Alma" , "12 avenue Janvier",
+									"7 rue Léon.B", "8 rue de la roseraie", "20 avenue Janvier", "1 place rosa parks"};
+		String gazolePrices[] = {"1,08 €", "1,10 €", "1,11 €" , "1,12 €", "1,08 €", "1,10 €", "1,11 €" , "1,12 €"};
+		String essencePrices[] = {"1,10 €", "1, 09 €","1,11 €" , "1,12 €", "1,08 €", "1,10 €", "1,11 €" , "1,12 €"};
+		
+		for (int i=0 ; i<8 ; i++){
+			String stationNameS = stationNames[i];
+			String stationAdressS = stationAdresss[i];
+			String gazolePriceS = gazolePrices[i];
+			String essencePriceS = essencePrices[i];
+			
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put(TAG_STATIONNAME, stationNameS);
+			map.put(TAG_STATIONADRESS, stationAdressS);
+			map.put(TAG_GAZOLEPRICE, gazolePriceS);
+			map.put(TAG_ESSENCEPRICE, essencePriceS);
+			stationsList.add(map);
+			list_stations = getListView();
+			ListAdapter adapter = new SimpleAdapter(getActivity(), stationsList,
+					R.layout.list_pattern,
+					new String[] { TAG_STATIONNAME,TAG_STATIONADRESS, TAG_GAZOLEPRICE, TAG_ESSENCEPRICE }, new int[] {
+					R.id.stationName,R.id.stationAdress, R.id.gazolePrice, R.id.essencePrice});
+			
+
+			list_stations.setAdapter(adapter);
+		}
+			
+					
+			list_stations.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					
+				}
+			});
+		
+	};
 
 	@Override  
 	public void onListItemClick(ListView l, View v, int position, long id) {  
