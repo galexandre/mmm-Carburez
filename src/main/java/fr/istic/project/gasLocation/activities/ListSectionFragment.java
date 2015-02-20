8b47a62850127c3f896471d306b2bedb67571c0f
Mathieu <mathieu.sachet@gmail.com>
2015-02-20 11:35:11 +0100

    Mise en place de la fiche station. Amélioration du design suivra.

--- src/main/java/fr/istic/project/gasLocation/activities/ListSectionFragment.java
+++ src/main/java/fr/istic/project/gasLocation/activities/ListSectionFragment.java
@@ -2,11 +2,15 @@ package fr.istic.project.gasLocation.activities;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,7 +22,6 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 import fr.istic.project.gasLocation.R;
 import fr.istic.project.gasLocation.models.Station;
 
@@ -26,66 +29,75 @@ public class ListSectionFragment extends ListFragment {
 
 	public static final String ARG_SECTION_NUMBER = "section_number";
 	
+	// ELEMENTS GRAPHIQUES DE L'ACTIVITE
 	public ListView list_stations;
 	private TextView stationName;
 	private TextView stationAdress;
 	private TextView gazolePrice;
-	private TextView essencePrice;
+	private TextView sp95Price;
 	private ImageView stationImage;
+	int logoStations[] = {R.drawable.access,R.drawable.leclerc,R.drawable.carrefour,R.drawable.elf,R.drawable.esso,R.drawable.total,R.drawable.access,R.drawable.elf};
 	
+	// TAGS UTILISES POUR LE HASHMAP
+	final String TAG_STATIONID = "id";
 	final String TAG_PICTURE = "thumbnail";
 	final String TAG_STATIONNAME = "stationName";
 	final String TAG_STATIONADRESS = "stationAdress";
 	final String TAG_GAZOLEPRICE = "gazolePrice";
-	final String TAG_ESSENCEPRICE = "essencePrice";
+	final String TAG_SP98PRICE = "sp98Price";
+	final String TAG_SP95PRICE = "sp95Price";
+	final String TAG_GPLPRICE = "gplPrice";
 	
-	private List<Station> currentStations;
+	private ArrayList<Station> currentStations;
 	HashMap<String, Object> map = new HashMap<String, Object>();
-	
 	ArrayList<HashMap<String, Object>> stationsList = new ArrayList<HashMap<String, Object>>();
 
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_section_list, container, false);
 		currentStations = getArguments().getParcelableArrayList("currentStations");
-        
 		stationImage = (ImageView) getActivity().findViewById(R.id.thumbnail);
 		stationName = (TextView) getActivity().findViewById(R.id.stationName);
 		stationAdress = (TextView) getActivity().findViewById(R.id.stationAdress);
 		gazolePrice = (TextView) getActivity().findViewById(R.id.gazolePrice);
-		essencePrice = (TextView) getActivity().findViewById(R.id.essencePrice);
-		
-		//image.setImageResource(R.drawable.total.);
-
+		sp95Price = (TextView) getActivity().findViewById(R.id.sp95Price);
 		return rootView;
 	}
 
+	
+	// CREATION DE LA LISTE SEULEMENT UNE FOIS L'ACTIVITE CREE (OBLIGATOIRE)
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
+
+		ArrayList<String> stationIds = new ArrayList<String>();
 		ArrayList<String> stationNames = new ArrayList<String>();
 		ArrayList<String> stationAdresss = new ArrayList<String>();
-		ArrayList<String> gazolePrices = new ArrayList<String>();
 		ArrayList<Map<String, Double>> hashMapGases = new ArrayList<Map<String, Double>>();
 		if (!(currentStations.isEmpty())){
 			for(Station s : currentStations){
+				stationIds.add(String.valueOf(s.getIdStation()));
 				stationNames.add(s.getTown());
 				stationAdresss.add(s.getAddress());
 				hashMapGases.add(s.getGases());
 			}
+			
+			// SI ON NE PARVIENT PAS A RECUPERER LES STATIONS, ON Y MET TEMPORAIREMENT DES STATIONS BIDONS
 		} else {
 			stationNames.add("Total Access");stationNames.add("E.Leclerc");stationNames.add("Carrefour");stationNames.add("ELF");stationNames.add("Esso");stationNames.add("ELF");
 			stationAdresss.add("11 rue du Général Leclerc");stationAdresss.add("6 rue de la gare");stationAdresss.add("11 rue de l'Alma");stationAdresss.add("20 Avenue Janvier");stationAdresss.add("1 Place Rosa Parks");
 		}
-		gazolePrices.add("1,08 €");gazolePrices.add("1,08 €");gazolePrices.add("1,08 €");gazolePrices.add("1,08 €");gazolePrices.add("1,08 €");gazolePrices.add("1,08 €");gazolePrices.add("1,08 €");		String essencePrices[] = {"1,10 €", "1, 09 €","1,11 €" , "1,12 €", "1,08 €", "1,10 €", "1,11 €" , "1,12 €"};
-		int pictures[] = {R.drawable.access,R.drawable.leclerc,R.drawable.carrefour,R.drawable.elf,R.drawable.esso,R.drawable.total,R.drawable.access,R.drawable.elf};
+
 		
 		for (int j=0 ; j<currentStations.size() ; j++){
+			String stationIdS = stationIds.get(j);
 			String stationNameS = stationNames.get(j);
 			String stationAdressS = stationAdresss.get(j);			
 			String gazolePriceS = "NC";
-			String essencePriceS = "NC";
+			String sp98PriceS = "NC";
+			String sp95PriceS = "NC";
+			String gplPriceS = "NC";
 			
 			Map<String, Double> gasesPrices = hashMapGases.get(j);
 			for (Map.Entry<String,Double> e : gasesPrices.entrySet()){
@@ -94,43 +106,57 @@ public class ListSectionFragment extends ListFragment {
 					gazolePriceS = e.getValue().toString();
 				}
 				else if (e.getKey().equals("SP98")){
-					essencePriceS = e.getValue().toString();
+					sp98PriceS = e.getValue().toString();
+				}
+				else if (e.getKey().equals("SP95")){
+					sp95PriceS = e.getValue().toString();
+				}
+				else if (e.getKey().equals("GPL")){
+					gplPriceS = e.getValue().toString();
 				}
 			}
+			
+			// ON PREND TEMPORAIREMENT UN LOGO D'ENSEIGNE AU HASARD
 			Random r = new Random();
 			int valeur = 0 + r.nextInt(7 - 0);
-			int pictureS = pictures[valeur];
-				
+			int pictureS = logoStations[valeur];
+			
+			// AJOUT DE TOUTES LES INFOS DES STATIONS DANS UN HASHMAP
 			HashMap<String, Object> map = new HashMap<String, Object>();
 			map.put(TAG_PICTURE, pictureS);
+			map.put(TAG_STATIONID, stationIdS);
 			map.put(TAG_STATIONNAME, stationNameS);
 			map.put(TAG_STATIONADRESS, stationAdressS);
 			map.put(TAG_GAZOLEPRICE, gazolePriceS);
-			map.put(TAG_ESSENCEPRICE, essencePriceS);
+			map.put(TAG_SP98PRICE, sp98PriceS);
+			map.put(TAG_SP95PRICE, sp95PriceS);
 			stationsList.add(map);
 			list_stations = getListView();
+			
+			// MISE EN PLACE D'UN ADAPTER POUR L'AFFICHAGE DE LA LISTE
 			ListAdapter adapter = new SimpleAdapter(getActivity(), stationsList,
 					R.layout.list_pattern,
-					new String[] {TAG_PICTURE, TAG_STATIONNAME,TAG_STATIONADRESS, TAG_GAZOLEPRICE, TAG_ESSENCEPRICE }, new int[] {
-					R.id.thumbnail, R.id.stationName,R.id.stationAdress, R.id.gazolePrice, R.id.essencePrice});
+					new String[] {TAG_PICTURE, TAG_STATIONNAME,TAG_STATIONADRESS, TAG_GAZOLEPRICE, TAG_SP98PRICE}, new int[] {
+					R.id.thumbnail, R.id.stationName,R.id.stationAdress, R.id.gazolePrice, R.id.sp95Price});
 			
-
 			list_stations.setAdapter(adapter);
 		}
 			
-					
+			// LORSQU'ON CLIQUE SUR UNE STATION DE LA LISTE
 			list_stations.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 				public void onItemClick(AdapterView<?> parent, View view,
 						int position, long id) {
-					
+					HashMap<String, Object> item = stationsList.get(position);
+					Intent intent = new Intent(getActivity(), StationActivity.class);
+					intent.putExtra("idStation", item.get("id").toString());
+					intent.putExtra("stationAdress", item.get("stationAdress").toString());
+					intent.putExtra("stationTown", item.get("stationName").toString());
+					intent.putExtra("gazolePrice", item.get("gazolePrice").toString());
+					//intent.putExtra("sp98price", item.get("sp98price").toString());
+					//intent.putExtra("sp95price", item.get("sp95price").toString());
+					//intent.putExtra("gplprice", item.get("gplprice").toString());
+					startActivity(intent);
 				}
 			});
-		
-	};
-
-	@Override  
-	public void onListItemClick(ListView l, View v, int position, long id) {  
-		Toast toast = Toast.makeText(super.getActivity().getApplicationContext(), "A cliqué !", Toast.LENGTH_SHORT);
-		toast.show();   
-	}   
+	}; 
 }
---
+++ src/main/java/fr/istic/project/gasLocation/activities/StationActivity.java
@@ -0,0 +1,53 @@
+package fr.istic.project.gasLocation.activities;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import fr.istic.project.gasLocation.R;
+import fr.istic.project.gasLocation.models.Station;
+
+public class StationActivity extends Activity {
+
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	private TextView stationTown;
+	private TextView stationAdress;
+	private TextView gazolePrice;
+	private TextView sp98price;
+	private TextView sp95price;
+	private TextView gplprice;
+	private ImageView logoStation;
+
+	private ArrayList<Station> currentStations;
+	private String idStation;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.station_activity);
+		Intent intent = getIntent();
+
+		stationTown = (TextView) findViewById(R.id.tv_town);
+		stationAdress = (TextView) findViewById(R.id.tv_adress);
+		gazolePrice = (TextView) findViewById(R.id.tv_gazolePrice);
+		sp95price = (TextView) findViewById(R.id.tv_sp95price);
+		sp98price = (TextView) findViewById(R.id.tv_sp98price);
+		gplprice = (TextView) findViewById(R.id.tv_gplprice);
+		logoStation = (ImageView) findViewById(R.id.logoStation);
+		logoStation.setImageResource(R.drawable.leclerc);
+
+		if (intent != null) {
+			stationAdress.setText(intent.getStringExtra("stationAdress"));
+			stationTown.setText(intent.getStringExtra("stationTown"));
+			gazolePrice.setText(intent.getStringExtra("gazolePrice"));
+			//sp98price.setText(intent.getStringExtra("sp98Price"));
+			//sp95price.setText(intent.getStringExtra("sp95Price"));
+			//gplprice.setText(intent.getStringExtra("gplprice"));
+		}
+	}
+}
