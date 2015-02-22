13cf0b346a190372d0cb2996a7517a849cb372f8
Corentin <corentin.clement.pro@gmail.com>
2015-02-22 21:03:39 +0100

    link to gps app

--- src/main/java/fr/istic/project/gasLocation/activities/StationActivity.java
+++ src/main/java/fr/istic/project/gasLocation/activities/StationActivity.java
@@ -1,11 +1,12 @@
 package fr.istic.project.gasLocation.activities;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import fr.istic.project.gasLocation.R;
@@ -16,7 +17,7 @@ public class StationActivity extends Activity {
 	public static final String ARG_SECTION_NUMBER = "section_number";
 
 	public static final String TAG = "StationActivity";
-	
+
 	private TextView stationTown;
 	private TextView stationAdress;
 	private TextView gazolePrice;
@@ -24,9 +25,10 @@ public class StationActivity extends Activity {
 	private TextView sp95price;
 	private TextView e85price;
 	private ImageView logoStation;
+	
+	private Station station;
 
-	private ArrayList<Station> currentStations;
-	private String idStation;
+	private Button bt_road;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,8 @@ public class StationActivity extends Activity {
 		setContentView(R.layout.station_activity);
 		Intent intent = getIntent();
 
+		// get widgets
+		bt_road = (Button) findViewById(R.id.bt_road);
 		logoStation = (ImageView) findViewById(R.id.logoStation);
 		logoStation.setImageResource(R.drawable.leclerc);
 		stationTown = (TextView) findViewById(R.id.tv_town);
@@ -43,18 +47,17 @@ public class StationActivity extends Activity {
 		sp98price = (TextView) findViewById(R.id.tv_sp98price);
 		e85price = (TextView) findViewById(R.id.tv_e85price);
 
-
+		// fill data
 		if (intent != null) {
-			
 			if (intent.getParcelableExtra("station")!=null) {
-				Station station = intent.getParcelableExtra("station");
-				
+				station = intent.getParcelableExtra("station");
+
 				// fill data on the UI
 				stationAdress.setText(station.getAddress());
 				stationTown.setText(station.getTown());
-				
+
 				if (station.getGases() != null) {
-					
+
 				} else {
 					gazolePrice.setText("?");
 					sp98price.setText("?");
@@ -63,5 +66,19 @@ public class StationActivity extends Activity {
 				}
 			}
 		}
+		
+		// events
+		bt_road.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Intent navigation = new Intent(Intent.ACTION_VIEW, Uri
+						.parse("http://maps.google.com/maps?saddr="
+								+ station.getLatitude() + "," + station.getLongitude()
+								+ "&daddr=" + station.getLatitude() + ","
+								+ station.getLongitude()));
+				startActivity(navigation);
+			}
+		});
+		
 	}
 }
