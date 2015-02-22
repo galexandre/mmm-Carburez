e9292db04bf26195aea213a6678f36dbf6946257
Corentin <corentin.clement.pro@gmail.com>
2015-02-22 11:52:22 +0100

    fix issue

--- src/main/java/fr/istic/project/gasLocation/controller/DownloadController.java
+++ src/main/java/fr/istic/project/gasLocation/controller/DownloadController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 
@@ -20,85 +21,88 @@ import fr.istic.project.gasLocation.services.UnzipImpl;
 import fr.istic.project.gasLocation.services.location.Localization;
 
 public class DownloadController {
-	 private Context myContext;
-	    private DataRetrieving dd;
-	    private Unzip uz;
-	    private Parser p;
-	    private String url;
-	    DatabaseHelper helper;
-	    private LocalizationInterface li;
-	    
-	    /**
-	     * Contructor of a controller
-	     * @param ctx a Context (android.content.Context)
-	     */
-	    public DownloadController(Context ctx, String url){
-	        this.myContext=ctx;
-	        this.url=url;
-	        li = new Localization(ctx);
-	        helper = OpenHelperManager.getHelper(myContext, DatabaseHelper.class);
-	    }
-	    
-	    public void execute(){
-	    	downloadData(url);
-	    	try {
-				unzipFile();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    	try {
-				parseXmlFile();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (XmlPullParserException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    	persistData();
-	    	deleteZipFile();
-	    }
-	    
-	    public DatabaseHelper getDatabaseHelper(){
-	    	return helper;
-	    }
-	    
-	    private void persistData(){
-	        ParserImpl p = getParser();
-	         for(fr.istic.project.gasLocation.services.Station s : p.getPvd()){
-	         	Station stat = new Station((double)s.getLatitude(), (double)s.getLongitude(), s.getZipcode(), s.getAdress(), s.getCity(), "", "", "", "", s.getSp95Prix(), s.getSp98Prix(), s.getE85Prix(), s.getGasolePrix(),  s.getGplPrix());
-	         	helper.addStation(stat);
-	         }
-	    }
+	
+	private static final String TAG = DownloadController.class.getName();
+	
+	private Context myContext;
+	private DataRetrieving dd;
+	private Unzip uz;
+	private Parser p;
+	private String url;
+	DatabaseHelper helper;
+	private LocalizationInterface li;
 
-	    /**
-	     * Donwload data from the url in parameter
-	     * @param url : String
-	     */
-	    public void downloadData(String url){
-	        dd = new DataRetrievingImpl(myContext,url);
-	        dd.download();
-	    }
+	/**
+	 * Contructor of a controller
+	 * @param ctx a Context (android.content.Context)
+	 */
+	public DownloadController(Context ctx, String url){
+		this.myContext=ctx;
+		this.url=url;
+		li = new Localization(ctx);
+		helper = OpenHelperManager.getHelper(myContext, DatabaseHelper.class);
+	}
 
-	    public void unzipFile() throws IOException {
-	        uz = new UnzipImpl("/Download","data.zip");
-	        uz.unzipData();
-	    }
+	public void execute(){
+		downloadData(url);
+		try {
+			unzipFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			parseXmlFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XmlPullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		persistData();
+		deleteZipFile();
+	}
 
-	    public void deleteZipFile(){
-	        uz.deleteZipFile();
-	    }
+	public DatabaseHelper getDatabaseHelper(){
+		return helper;
+	}
 
-	    public void parseXmlFile() throws IOException, XmlPullParserException {
-	        p = new ParserImpl(uz.getNameOftheUnzipFile(),li);
-	        p.parse();
-	    }
-	    
-	    public ParserImpl getParser()
-	    {
-	    	return (ParserImpl)p;
-	    }
-	    
-	    
+	private void persistData(){
+		ParserImpl p = getParser();
+		for(fr.istic.project.gasLocation.services.Station s : p.getPvd()){
+			Station stat = new Station((double)s.getLatitude(), (double)s.getLongitude(), s.getZipcode(), s.getAdress(), s.getCity(), "", "", "", "", s.getSp95Prix(), s.getSp98Prix(), s.getE85Prix(), s.getGasolePrix(),  s.getGplPrix());
+			helper.addStation(stat);
+		}
+	}
+
+	/**
+	 * Donwload data from the url in parameter
+	 * @param url : String
+	 */
+	public void downloadData(String url){
+		dd = new DataRetrievingImpl(myContext,url);
+		dd.download();
+	}
+
+	public void unzipFile() throws IOException {
+		uz = new UnzipImpl("/Download","data.zip");
+		uz.unzipData();
+	}
+
+	public void deleteZipFile(){
+		uz.deleteZipFile();
+	}
+
+	public void parseXmlFile() throws IOException, XmlPullParserException {
+		p = new ParserImpl(uz.getNameOftheUnzipFile(),li);
+		p.parse();
+	}
+
+	public ParserImpl getParser()
+	{
+		return (ParserImpl)p;
+	}
+
+
 }
